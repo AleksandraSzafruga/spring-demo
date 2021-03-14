6b0684981c5345ea794f6497d6543244466623df
Aleksandra Szafruga <ola.szafruga@gmail.com>
2021-03-14 14:28:53 +0100

    [change] - application properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO BOOKS (IMIĘ, SURNAME, TYTUŁ)
+VALUES ('Henryk', 'Sienkiewicz', 'Potop');
+
+INSERT INTO BOOKS (IMIĘ, SURNAME, TYTUŁ)
+VALUES ('Henryk', 'Sienkiewicz', 'Pan Wołodyjowski');
+
+INSERT INTO BOOKS (IMIĘ, SURNAME, TYTUŁ)
+VALUES ('Henryk', 'Sienkiewicz', 'Ogniem i mieczem');
